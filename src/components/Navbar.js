c5a129710226c1f8787e9c23bcc770ef1e619f7c
shv14 <142407244+shv14@users.noreply.github.com>
2024-01-29 01:07:21 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,14 +8,14 @@ export default function Navbar(props) {
     return (
         <nav className="navbar navbar-expand-lg text-dark font-weight-bold" style={{ backgroundColor: "#6dc09a" }} >
             <div className="container-fluid">
-                <a className="navbar-brand font-weight-bolder" href="/">{props.title}</a>
+                <a className="navbar-brand font-weight-bolder" href="/mern-notes-frontend">{props.title}</a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                         <li className="nav-item">
-                            <Link className={`nav-link ${location.pathname === "/" ? "active" : ""}`} aria-current="page" to="/">Home</Link>
+                            <Link className={`nav-link ${location.pathname === "/mern-notes-frontend" ? "active" : ""}`} aria-current="page" to="/mern-notes-frontend">Home</Link>
                         </li>
                         <li className="nav-item">
                             <Link className={`nav-link ${location.pathname === "/about" ? "active" : ""}`} to="/about">{props.Page1}</Link>
